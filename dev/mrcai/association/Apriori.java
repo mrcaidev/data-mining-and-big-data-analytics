81c6bfeb8d43ed9b05eccb25a5c274d9e0afc55d
mrcaidev <mrcaidev@gmail.com>
2022-12-14 01:17:46 +0800

    refactor: better code quality

--- dev/mrcai/association/Apriori.java
+++ dev/mrcai/association/Apriori.java
@@ -11,51 +11,47 @@ import java.util.Map;
 import java.util.Set;
 
 public class Apriori {
+    private List<Set<String>> itemSets;
     private double minSupport;
     private double minConfidence;
-    private List<Set<String>> dataSet;
 
     public static void main(String[] args) {
-        Apriori apriori = new Apriori(0.1, 0.5);
+        Apriori apriori = new Apriori("data/association/top1000data.txt", 0.1, 0.5);
         apriori.go();
     }
 
-    Apriori(double minSupport, double minConfidence) {
+    Apriori(String filePath, double minSupport, double minConfidence) {
+        itemSets = loadItemSets(filePath);
         this.minSupport = minSupport;
         this.minConfidence = minConfidence;
-        this.dataSet = Apriori.loadDataSet("data/association/top1000data.txt");
     }
 
-    /**
-     * Run the Apriori algorithm.
-     */
     public void go() {
         // Record all frequent item sets.
-        List<Map<Set<String>, Double>> allFrequentItemSetsList = new ArrayList<Map<Set<String>, Double>>();
+        List<Map<Set<String>, Double>> frequentItemSetsList = new ArrayList<Map<Set<String>, Double>>();
 
         // Start with frequent 1 item sets.
-        allFrequentItemSetsList.add(this.getFrequentOneItemSets());
+        frequentItemSetsList.add(getFrequentOneItemSets());
 
         while (true) {
             // Get the frequent k-1 item sets.
-            Map<Set<String>, Double> lastFrequentItemSets = allFrequentItemSetsList
-                    .get(allFrequentItemSetsList.size() - 1);
+            Map<Set<String>, Double> frequentLastItemSets = frequentItemSetsList.get(frequentItemSetsList.size() - 1);
 
             // Get the frequent k item sets.
-            Map<Set<String>, Double> frequentItemSets = this.getFrequentItemSets(lastFrequentItemSets);
+            Map<Set<String>, Double> frequentKItemSets = getFrequentKItemSets(frequentLastItemSets);
 
             // If there is no frequent k item set, stop exploring.
-            if (frequentItemSets.isEmpty()) {
+            if (frequentKItemSets.isEmpty()) {
                 break;
             }
 
             // Otherwise, record the frequent k item sets.
-            allFrequentItemSetsList.add(frequentItemSets);
+            frequentItemSetsList.add(frequentKItemSets);
         }
 
         // Flatten all frequent item sets into a single map.
         Map<Set<String>, Double> allFrequentItemSets = new HashMap<Set<String>, Double>();
-        for (Map<Set<String>, Double> frequentItemSets : allFrequentItemSetsList) {
+        for (Map<Set<String>, Double> frequentItemSets : frequentItemSetsList) {
             allFrequentItemSets.putAll(frequentItemSets);
         }
 
@@ -67,7 +63,7 @@ public class Apriori {
         }
 
         // Get all association rules.
-        Map<Map<Set<String>, Set<String>>, Double> associationRules = this.getAssociationRules(allFrequentItemSets);
+        Map<Map<Set<String>, Set<String>>, Double> associationRules = getAssociationRules(allFrequentItemSets);
 
         // Print all association rules.
         System.out.println("\nAll association rules:\n");
@@ -79,132 +75,109 @@ public class Apriori {
         }
     }
 
-    /**
-     * Load data set from a file.
-     *
-     * @param filePath The path of the file.
-     * @return A list of data. Each data is a set of items.
-     */
-    private static List<Set<String>> loadDataSet(String filePath) {
-        List<Set<String>> dataSet = new ArrayList<Set<String>>();
-
-        try {
-            FileReader fileReader = new FileReader(filePath);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-
-            String line = null;
-            while ((line = bufferedReader.readLine()) != null) {// 读一行文件
-                List<String> rawData = Arrays.asList(line.trim().split(","));
-                Set<String> data = new HashSet<String>(rawData);
-                dataSet.add(data);
-            }
-
-            bufferedReader.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return dataSet;
-    }
-
-    /**
-     * Get all frequent 1 item sets.
-     *
-     * @return A map, whose key is a frequent 1 item set, and value is its
-     *         corresponding support value.
-     */
     private Map<Set<String>, Double> getFrequentOneItemSets() {
+        // Record all frequent 1 item sets, and their corresponding support values.
         Map<Set<String>, Double> frequentOneItemSets = new HashMap<Set<String>, Double>();
+
+        // Record all 1 item sets that have been counted, to avoid duplicate counting.
         Set<Set<String>> countedOneItemSets = new HashSet<Set<String>>();
 
-        for (Set<String> data : this.dataSet) {
-            for (String item : data) {
+        for (Set<String> itemSet : itemSets) {
+            for (String item : itemSet) {
                 // Treat each single item as a 1 item set.
-                Set<String> itemSet = new HashSet<String>();
-                itemSet.add(item);
+                Set<String> oneItemSet = new HashSet<String>();
+                oneItemSet.add(item);
 
                 // If this 1 item set has been counted, skip it.
-                if (countedOneItemSets.contains(itemSet)) {
+                if (countedOneItemSets.contains(oneItemSet)) {
                     continue;
                 }
 
                 // If this 1 item set is frequent, record it.
-                double support = this.getSupport(itemSet);
-                if (support >= this.minSupport) {
-                    frequentOneItemSets.put(itemSet, support);
+                double support = getSupport(oneItemSet);
+                if (support >= minSupport) {
+                    frequentOneItemSets.put(oneItemSet, support);
                 }
 
                 // Mark this 1 item set as counted.
-                countedOneItemSets.add(itemSet);
+                countedOneItemSets.add(oneItemSet);
             }
         }
 
         return frequentOneItemSets;
     }
 
-    /**
-     * Get all frequent k item sets based on the frequent k-1 item sets.
-     *
-     * @param lastFrequentItemSets The frequent k-1 item sets.
-     * @return A map, whose key is a frequent k item set, and value is its
-     *         corresponding support value.
-     */
-    private Map<Set<String>, Double> getFrequentItemSets(Map<Set<String>, Double> lastFrequentItemSets) {
-        Map<Set<String>, Double> frequentItemSets = new HashMap<Set<String>, Double>();
+    private Map<Set<String>, Double> getFrequentKItemSets(Map<Set<String>, Double> frequentLastItemSets) {
+        // Record all frequent k item sets, and their corresponding support values.
+        Map<Set<String>, Double> frequentKItemSets = new HashMap<Set<String>, Double>();
+
+        // Record all k item sets that have been counted, to avoid duplicate counting.
+        Set<Set<String>> countedKItemSets = new HashSet<Set<String>>();
 
         // For every two k-1 frequent item sets.
-        for (Set<String> itemSet1 : lastFrequentItemSets.keySet()) {
-            for (Set<String> itemSet2 : lastFrequentItemSets.keySet()) {
+        for (Set<String> frequentLastItemSet1 : frequentLastItemSets.keySet()) {
+            for (Set<String> frequentLastItemSet2 : frequentLastItemSets.keySet()) {
                 // If the two k-1 item sets are the same, skip it.
-                if (itemSet1.equals(itemSet2)) {
+                if (frequentLastItemSet1.equals(frequentLastItemSet2)) {
                     continue;
                 }
 
                 // Merge the two k-1 item sets into a new item set.
-                Set<String> kItemSet = new HashSet<String>(itemSet1);
-                kItemSet.addAll(itemSet2);
+                Set<String> kItemSet = new HashSet<String>(frequentLastItemSet1);
+                kItemSet.addAll(frequentLastItemSet2);
 
                 // If the new item set is not a k item set, skip it.
-                if (kItemSet.size() != itemSet1.size() + 1) {
+                if (kItemSet.size() != frequentLastItemSet1.size() + 1) {
+                    continue;
+                }
+
+                // If the k item set has been counted, skip it.
+                if (countedKItemSets.contains(kItemSet)) {
                     continue;
                 }
 
                 // If the k item set has an infrequent k-1 subset, skip it.
-                if (Apriori.hasInfrequentSubset(kItemSet, lastFrequentItemSets)) {
+                if (hasInfrequentSubset(kItemSet, frequentLastItemSets)) {
                     continue;
                 }
 
                 // If the merged item set is frequent, record it.
-                double support = this.getSupport(kItemSet);
-                if (support >= this.minSupport) {
-                    frequentItemSets.put(kItemSet, support);
+                double support = getSupport(kItemSet);
+                if (support >= minSupport) {
+                    frequentKItemSets.put(kItemSet, support);
                 }
+
+                // Mark this k item set as counted.
+                countedKItemSets.add(kItemSet);
             }
         }
 
-        return frequentItemSets;
+        return frequentKItemSets;
     }
 
-    /**
-     * Check if a given k item set has an infrequent k-1 subset.
-     *
-     * @param itemSet              The given k item set.
-     * @param lastFrequentItemSets The frequent k-1 item sets.
-     * @return True if the given k item set has an infrequent k-1 subset, or false
-     *         otherwise.
-     */
-    private static boolean hasInfrequentSubset(Set<String> itemSet, Map<Set<String>, Double> lastFrequentItemSets) {
+    private static boolean hasInfrequentSubset(Set<String> itemSet, Map<Set<String>, Double> frequentLastItemSets) {
         // For every k-1 subset of the given item set.
         for (String item : itemSet) {
             Set<String> subset = new HashSet<String>(itemSet);
             subset.remove(item);
 
             // If this subset is not frequent, return true.
-            if (!lastFrequentItemSets.containsKey(subset)) {
+            if (!frequentLastItemSets.containsKey(subset)) {
                 return true;
             }
         }
         return false;
+    }
+
+    private double getSupport(Set<String> itemSet) {
+        double support = 0.0;
+        for (Set<String> realItemSet : itemSets) {
+            if (!realItemSet.containsAll(itemSet)) {
+                continue;
+            }
+            support += 1;
+        }
+        return support / itemSets.size();
     }
 
     /**
@@ -217,35 +190,14 @@ public class Apriori {
      *         association rules are {{A} -> {B, C}, {B} -> {A, C}, {C} -> {A, B},
      *         {A, B} -> {C}, {A, C} -> {B}, {B, C} -> {A}}.
      */
-    private static Map<Set<String>, Set<String>> getCandidateRules(Set<String> frequentItemSet) {
-        Map<Set<String>, Set<String>> candidateRules = new HashMap<Set<String>, Set<String>>();
-
-        // For every possible condition size.
-        for (int i = 1; i < frequentItemSet.size(); i++) {
-            // Get all possible conditions.
-            Set<Set<String>> conditions = Apriori.getCombinations(frequentItemSet, i);
-
-            // For every possible condition.
-            for (Set<String> condition : conditions) {
-                // Get the result.
-                Set<String> result = new HashSet<String>(frequentItemSet);
-                result.removeAll(condition);
-
-                // Record the association rule.
-                candidateRules.put(condition, result);
-            }
-        }
-        return candidateRules;
-    }
-
-    private Map<Map<Set<String>, Set<String>>, Double> getAssociationRules(
-            Map<Set<String>, Double> allFrequentItemSets) {
+    private Map<Map<Set<String>, Set<String>>, Double> getAssociationRules(Map<Set<String>, Double> frequentItemSets) {
+        // Record all association rules, and their corresponding confidence values.
         Map<Map<Set<String>, Set<String>>, Double> associationRules = new HashMap<Map<Set<String>, Set<String>>, Double>();
 
         // For every frequent item set.
-        for (Set<String> frequentItemSet : allFrequentItemSets.keySet()) {
+        for (Set<String> frequentItemSet : frequentItemSets.keySet()) {
             // Get all possible association rules.
-            Map<Set<String>, Set<String>> candidateRules = Apriori.getCandidateRules(frequentItemSet);
+            Map<Set<String>, Set<String>> candidateRules = getPossibleAssociationRules(frequentItemSet);
 
             // For every possible association rule.
             for (Set<String> condition : candidateRules.keySet()) {
@@ -253,10 +205,10 @@ public class Apriori {
                 Set<String> result = candidateRules.get(condition);
 
                 // Get the confidence.
-                double confidence = allFrequentItemSets.get(frequentItemSet) / allFrequentItemSets.get(condition);
+                double confidence = frequentItemSets.get(frequentItemSet) / frequentItemSets.get(condition);
 
                 // If the confidence is not high enough, skip it.
-                if (confidence < this.minConfidence) {
+                if (confidence < minConfidence) {
                     continue;
                 }
 
@@ -268,6 +220,39 @@ public class Apriori {
         }
 
         return associationRules;
+    }
+
+    /**
+     * Get all possible association rules based on the given frequent item set.
+     *
+     * @param frequentItemSet The frequent item set.
+     * @return A map, whose key is a condition, and value is its result.
+     *
+     *         For example, if the given frequent item set is {A, B, C}, the
+     *         association rules are {{A} -> {B, C}, {B} -> {A, C}, {C} -> {A, B},
+     *         {A, B} -> {C}, {A, C} -> {B}, {B, C} -> {A}}.
+     */
+    private static Map<Set<String>, Set<String>> getPossibleAssociationRules(Set<String> frequentItemSet) {
+        // Record all possible association rules.
+        Map<Set<String>, Set<String>> rules = new HashMap<Set<String>, Set<String>>();
+
+        // For every possible condition size.
+        for (int i = 1; i < frequentItemSet.size(); i++) {
+            // Get all possible conditions.
+            Set<Set<String>> conditions = getCombinations(frequentItemSet, i);
+
+            // For every possible condition.
+            for (Set<String> condition : conditions) {
+                // Get the result.
+                Set<String> result = new HashSet<String>(frequentItemSet);
+                result.removeAll(condition);
+
+                // Record the association rule.
+                rules.put(condition, result);
+            }
+        }
+
+        return rules;
     }
 
     /**
@@ -300,7 +285,7 @@ public class Apriori {
             remainingElements.remove(element);
 
             // Get all possible combinations of the remaining elements.
-            Set<Set<String>> remainingCombinations = Apriori.getCombinations(remainingElements, size - 1);
+            Set<Set<String>> remainingCombinations = getCombinations(remainingElements, size - 1);
 
             // For every possible combination of the remaining elements.
             for (Set<String> remainingCombination : remainingCombinations) {
@@ -316,20 +301,25 @@ public class Apriori {
         return combinations;
     }
 
-    /**
-     * Get the support value of a given item set.
-     *
-     * @param itemSet The given item set.
-     * @return The support value of the given item set.
-     */
-    private double getSupport(Set<String> itemSet) {
-        double support = 0.0;
-        for (Set<String> data : this.dataSet) {
-            if (!data.containsAll(itemSet)) {
-                continue;
+    private static List<Set<String>> loadItemSets(String filePath) {
+        List<Set<String>> itemSets = new ArrayList<Set<String>>();
+
+        try {
+            FileReader fileReader = new FileReader(filePath);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String line = null;
+            while ((line = bufferedReader.readLine()) != null) {
+                List<String> itemList = Arrays.asList(line.trim().split(","));
+                Set<String> itemSet = new HashSet<String>(itemList);
+                itemSets.add(itemSet);
             }
-            support += 1;
+
+            bufferedReader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return support / this.dataSet.size();
+
+        return itemSets;
     }
 }
