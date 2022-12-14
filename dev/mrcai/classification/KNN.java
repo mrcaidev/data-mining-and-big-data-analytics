689de985a7e2d4ff59944561680c2c86c044e3be
mrcaidev <mrcaidev@gmail.com>
2022-12-14 15:53:19 +0800

    refactor: better code quality

--- dev/mrcai/classification/KNN.java
+++ dev/mrcai/classification/KNN.java
@@ -12,13 +12,104 @@ public class KNN {
     int k;
 
     public static void main(String[] args) {
-        KNN knn = new KNN(3);
-        knn.predict();
+        KNN knn = new KNN("data/classification/train.csv", 3);
+        knn.predict("data/classification/test.csv");
     }
 
-    public KNN(int k) {
-        this.trainSet = KNN.loadDataSet("data/classification/train.csv");
+    public KNN(String filePath, int k) {
+        trainSet = loadDataSet(filePath);
         this.k = k;
+    }
+
+    public void predict(String filePath) {
+        // Read test data set.
+        List<Data> testSet = loadDataSet(filePath);
+
+        // Predict each data in test data set.
+        int correctCount = 0;
+        for (Data testData : testSet) {
+            String prediction = classify(testData);
+            if (prediction.equals(testData.label)) {
+                correctCount++;
+                continue;
+            }
+            System.out.println("Wrong prediction: " + prediction + ", Actual: " + testData.label);
+        }
+
+        // Print accuracy.
+        double accuracy = correctCount * 100.0 / testSet.size();
+        System.out.println("Accuracy: " + accuracy + "%");
+    }
+
+    public String classify(Data testData) {
+        // Calculate the distance between the test data and each training data.
+        double[] distances = getDistances(testData);
+
+        // Record the labels of the k nearest neighbors,
+        // and the times they appear.
+        Map<String, Integer> neighbors = new HashMap<String, Integer>();
+
+        for (int num = 0; num < k; num++) {
+            // Find the nearest neighbor.
+            double minDistance = Integer.MAX_VALUE;
+            int minIndex = -1;
+            for (int i = 0; i < distances.length; i++) {
+                if (distances[i] >= minDistance) {
+                    continue;
+                }
+                minDistance = distances[i];
+                minIndex = i;
+            }
+
+            // If the nearest neighbor is not in the map, break the loop.
+            if (minIndex == -1) {
+                break;
+            }
+
+            // Exclude this neighbor from the next search.
+            distances[minIndex] = Integer.MAX_VALUE;
+
+            // Record the label of the nearest neighbor.
+            String label = trainSet.get(minIndex).label;
+            if (neighbors.containsKey(label)) {
+                neighbors.put(label, neighbors.get(label) + 1);
+            } else {
+                neighbors.put(label, 1);
+            }
+        }
+
+        return getMostFrequentLabel(neighbors);
+    }
+
+    private double[] getDistances(Data testData) {
+        double[] distances = new double[trainSet.size()];
+        for (int i = 0; i < trainSet.size(); i++) {
+            distances[i] = getDistance(testData, trainSet.get(i));
+        }
+        return distances;
+    }
+
+    private String getMostFrequentLabel(Map<String, Integer> neighbors) {
+        String mostFrequentLabel = "";
+        int maxCount = 0;
+        for (String label : neighbors.keySet()) {
+            int count = neighbors.get(label);
+            if (count <= maxCount) {
+                continue;
+            }
+            maxCount = count;
+            mostFrequentLabel = label;
+        }
+        return mostFrequentLabel;
+    }
+
+    private static double getDistance(Data testData, Data trainData) {
+        // Calculate the Euclidean distance.
+        double sum = 0;
+        for (int i = 0; i < testData.data.length; i++) {
+            sum += Math.pow(testData.data[i] - trainData.data[i], 2);
+        }
+        return Math.sqrt(sum);
     }
 
     private static List<Data> loadDataSet(String filePath) {
@@ -44,87 +135,5 @@ public class KNN {
         }
 
         return dataSet;
-    }
-
-    public void predict() {
-        // Read test data set.
-        List<Data> testSet = KNN.loadDataSet("data/classification/test.csv");
-
-        // Predict each data in test data set.
-        int correct = 0;
-        for (Data data : testSet) {
-            String prediction = this.classify(data);
-            if (prediction.equals(data.label)) {
-                correct++;
-                continue;
-            }
-            System.out.println("Wrong prediction: " + prediction + ", Actual: " + data.label);
-        }
-
-        // Print accuracy.
-        double accuracy = (double) correct / testSet.size() * 100;
-        System.out.println("Accuracy: " + accuracy + "%");
-    }
-
-    public String classify(Data testData) {
-        // Calculate the distance between the test data and each training data.
-        double[] distances = new double[this.trainSet.size()];
-        for (int i = 0; i < this.trainSet.size(); i++) {
-            distances[i] = KNN.getDistance(testData, this.trainSet.get(i));
-        }
-
-        // Sort the distances and get the k nearest neighbors.
-        Map<String, Integer> voteMap = new HashMap<String, Integer>();
-        for (int num = 0; num < this.k; num++) {
-            // Find the nearest neighbor.
-            double minDistance = Integer.MAX_VALUE;
-            int minIndex = -1;
-            for (int i = 0; i < this.trainSet.size(); i++) {
-                if (distances[i] >= minDistance) {
-                    continue;
-                }
-                minDistance = distances[i];
-                minIndex = i;
-            }
-
-            // If the nearest neighbor is not in the map, break the loop.
-            if (minIndex == -1) {
-                break;
-            }
-
-            // Exclude this neighbor from the next search.
-            distances[minIndex] = Integer.MAX_VALUE;
-
-            // Add the label of the nearest neighbor to the vote map.
-            String label = this.trainSet.get(minIndex).label;
-            if (voteMap.containsKey(label)) {
-                voteMap.put(label, voteMap.get(label) + 1);
-            } else {
-                voteMap.put(label, 1);
-            }
-        }
-
-        // Find the label with the most votes.
-        String prediction = "";
-        int maxVote = 0;
-        for (String label : voteMap.keySet()) {
-            int vote = voteMap.get(label);
-            if (vote <= maxVote) {
-                continue;
-            }
-            maxVote = vote;
-            prediction = label;
-        }
-
-        return prediction;
-    }
-
-    private static double getDistance(Data testData, Data trainData) {
-        // Calculate the Euclidean distance.
-        double sum = 0;
-        for (int i = 0; i < testData.data.length; i++) {
-            sum += Math.pow(testData.data[i] - trainData.data[i], 2);
-        }
-        return Math.sqrt(sum);
     }
 }
