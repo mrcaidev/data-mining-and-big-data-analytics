ab89b034d8b7599b218337d7ff261521a0eb1dc7
mrcaidev <mrcaidev@gmail.com>
2022-12-18 17:34:58 +0800

    refactor: use PriorityQueue to find k closest data

--- dev/mrcai/datamining/classification/KNN.java
+++ dev/mrcai/datamining/classification/KNN.java
@@ -1,139 +1,129 @@
 package dev.mrcai.datamining.classification;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
 
 public class KNN {
-    private List<Data> trainSet;
+
+    /**
+     * When finding the k closest irises,
+     * each train iris is regarded as a neighbor.
+     */
+    private static class Neighbor {
+        public Iris iris;
+        public double distance;
+
+        public Neighbor(Iris iris, double distance) {
+            this.iris = iris;
+            this.distance = distance;
+        }
+    }
+
+    private List<Iris> trainIrises;
     private int k;
 
     public static void main(String[] args) {
         KNN knn = new KNN("data/classification/train.csv", 3);
-        knn.predict("data/classification/test.csv");
+        knn.test("data/classification/test.csv");
     }
 
     public KNN(String filePath, int k) {
-        trainSet = loadDataSet(filePath);
+        trainIrises = IOUtils.loadIrises(filePath);
         this.k = k;
     }
 
-    public void predict(String filePath) {
-        // Read test data set.
-        List<Data> testSet = loadDataSet(filePath);
+    public void test(String filePath) {
+        // Load test irises.
+        List<Iris> testIrises = IOUtils.loadIrises(filePath);
 
-        // Predict each data in test data set.
+        // Classify each iris.
         int correctCount = 0;
-        for (Data testData : testSet) {
-            String prediction = classify(testData);
-            if (prediction.equals(testData.label)) {
+        for (Iris testIris : testIrises) {
+            String prediction = classify(testIris);
+            if (testIris.getLabel().equals(prediction)) {
                 correctCount++;
                 continue;
             }
-            System.out.println("Wrong prediction: " + prediction + ", Actual: " + testData.label);
+            IOUtils.printPrediction(testIris, prediction);
         }
 
-        // Print accuracy.
-        double accuracy = correctCount * 100.0 / testSet.size();
-        System.out.println("Accuracy: " + accuracy + "%");
+        // Print the accuracy.
+        double accuracy = correctCount * 1.0 / testIrises.size();
+        IOUtils.printAccuracy(accuracy);
     }
 
-    public String classify(Data testData) {
-        // Calculate the distance between the test data and each training data.
-        double[] distances = getDistances(testData);
+    private String classify(Iris iris) {
+        List<Iris> kClosestIrises = getKClosestIrises(iris);
+        return getMostFrequentLabel(kClosestIrises);
+    }
 
-        // Record the labels of the k nearest neighbors,
-        // and the times they appear.
-        Map<String, Integer> neighbors = new HashMap<String, Integer>();
+    private List<Iris> getKClosestIrises(Iris iris) {
+        // Store the k closest irises.
+        // The closest iris is at the tail of the queue.
+        PriorityQueue<Neighbor> neighbors = new PriorityQueue<Neighbor>(k, new Comparator<Neighbor>() {
+            @Override
+            public int compare(Neighbor o1, Neighbor o2) {
+                return -Double.compare(o1.distance, o2.distance);
+            }
+        });
 
-        for (int num = 0; num < k; num++) {
-            // Find the nearest neighbor.
-            double minDistance = Integer.MAX_VALUE;
-            int minIndex = -1;
-            for (int i = 0; i < distances.length; i++) {
-                if (distances[i] >= minDistance) {
-                    continue;
-                }
-                minDistance = distances[i];
-                minIndex = i;
+        // Find the k closest irises.
+        for (Iris trainIris : trainIrises) {
+            double distance = trainIris.getDistance(iris);
+
+            // If the queue is not full, add the neighbor.
+            if (neighbors.size() < k) {
+                neighbors.add(new Neighbor(trainIris, distance));
+                continue;
             }
 
-            // If the nearest neighbor is not in the map, break the loop.
-            if (minIndex == -1) {
-                break;
+            // If the neighbor is farther, skip it.
+            if (distance >= neighbors.peek().distance) {
+                continue;
             }
 
-            // Exclude this neighbor from the next search.
-            distances[minIndex] = Integer.MAX_VALUE;
+            // Otherwise, remove the farthest neighbor and add this one.
+            neighbors.poll();
+            neighbors.add(new Neighbor(trainIris, distance));
+        }
 
-            // Record the label of the nearest neighbor.
-            String label = trainSet.get(minIndex).label;
-            if (neighbors.containsKey(label)) {
-                neighbors.put(label, neighbors.get(label) + 1);
+        // Transform queue to list.
+        List<Iris> kClosestIrises = new ArrayList<Iris>();
+        while (!neighbors.isEmpty()) {
+            kClosestIrises.add(neighbors.poll().iris);
+        }
+
+        return kClosestIrises;
+    }
+
+    private static String getMostFrequentLabel(List<Iris> irises) {
+        // Count the number of each label.
+        Map<String, Integer> labelCount = new HashMap<String, Integer>();
+        for (Iris iris : irises) {
+            String label = iris.getLabel();
+            if (labelCount.containsKey(label)) {
+                labelCount.put(label, labelCount.get(label) + 1);
             } else {
-                neighbors.put(label, 1);
+                labelCount.put(label, 1);
             }
         }
 
-        return getMostFrequentLabel(neighbors);
-    }
-
-    private double[] getDistances(Data testData) {
-        double[] distances = new double[trainSet.size()];
-        for (int i = 0; i < trainSet.size(); i++) {
-            distances[i] = getDistance(testData, trainSet.get(i));
-        }
-        return distances;
-    }
-
-    private String getMostFrequentLabel(Map<String, Integer> neighbors) {
-        String mostFrequentLabel = "";
+        // Find the most frequent label.
         int maxCount = 0;
-        for (String label : neighbors.keySet()) {
-            int count = neighbors.get(label);
+        String mostFrequentLabel = null;
+        for (String label : labelCount.keySet()) {
+            int count = labelCount.get(label);
             if (count <= maxCount) {
                 continue;
             }
             maxCount = count;
             mostFrequentLabel = label;
         }
+
         return mostFrequentLabel;
-    }
-
-    private static double getDistance(Data testData, Data trainData) {
-        // Calculate the Euclidean distance.
-        double sum = 0;
-        for (int i = 0; i < testData.data.length; i++) {
-            sum += Math.pow(testData.data[i] - trainData.data[i], 2);
-        }
-        return Math.sqrt(sum);
-    }
-
-    private static List<Data> loadDataSet(String filePath) {
-        List<Data> dataSet = new ArrayList<Data>();
-
-        try {
-            FileReader fileReader = new FileReader(filePath);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-
-            String line = null;
-            while ((line = bufferedReader.readLine()) != null) {
-                String[] rawData = line.trim().split(",");
-                double[] data = new double[rawData.length - 1];
-                for (int i = 0; i < rawData.length - 1; i++) {
-                    data[i] = Double.parseDouble(rawData[i]);
-                }
-                dataSet.add(new Data(data, rawData[rawData.length - 1]));
-            }
-
-            bufferedReader.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return dataSet;
     }
 }
